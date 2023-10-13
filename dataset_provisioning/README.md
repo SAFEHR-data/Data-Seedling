be724048e9ff6158ca3d69cf5146696aa3eb963c
Tanya Borisova <tborisova@microsoft.com>
2023-10-13 15:18:15 +0000

    Add dataset provisioning pipeline (still need to write docs)

---
+++ dataset_provisioning/README.md
@@ -0,0 +1,12 @@
+# Dataset Provisioning for TRE Workspace
+
+This is a Dataset Provisioning pipeline. It provides a mechanism to provision datasets created within FlowEHR and into Azure TRE workspaces. 
+This example pipeline copies data created within Databricks Unity Catalogue (see TODO link) into an Azure TRE workspace.
+
+To be able to use Dataset Provisioning pipeline, a special TRE Workspace needs to be used. See a template available here: TODO.
+
+# How to set up
+
+Deploy the pipeline as normal. The pipeline can then be triggered from the ADF instance, as shown:
+
+TODO screenshot
---
+++ dataset_provisioning/dataset_provisioning.py
@@ -0,0 +1,102 @@
+import argparse
+import base64
+import datetime
+from typing import Any
+
+from pyspark.sql.session import SparkSession
+
+
+# These constants need to be set accordingly to the TRE Workspace template used (see README.md)
+DEFAULT_CONTAINER_NAME = "datalake"
+DEFAULT_STORAGE_ACCOUNT_PREFIX = "stgws"
+
+
+def parse_args() -> Any:
+    """
+    Uses argparse to parse arguments from command line.
+
+    :returns: An object with parsed arguments as attributes.
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--query_base64",
+        type=str,
+        help="Query to run for dataset extraction, encoded in base64. Example of a query: \"SELECT * from catalog.schema.table\"",
+    )
+    parser.add_argument(
+        "--dataset_name",
+        type=str,
+        help="Name of the dataset to provision in TRE Workspace storage",
+    )
+    parser.add_argument(
+        "--workspace_id", type=str, help="TRE Workspace ID to provision the dataset in"
+    )
+
+    return parser.parse_args()
+
+
+def set_up_auth_for_storage_account(spark: SparkSession, storage_account: str) -> None:
+    """
+    Sets up authentication to be able to use storage account in Workspace.
+    Required secrets (AAD Tenant ID, App ID and App Secret) are deployed with FlowEHR
+
+    Parameters:
+    :param spark: Spark session
+    :param storage_account: Storage account to set up auth for
+    """
+    directory_id = spark.conf.get("spark.secret.azure-tenant-id")
+    app_id = spark.conf.get("spark.secret.external-connection-app-id")
+    app_secret = spark.conf.get("spark.secret.exernal-connection-app-secret")
+
+    spark.conf.set(
+        f"fs.azure.account.auth.type.{storage_account}.dfs.core.windows.net", "OAuth"
+    )
+    spark.conf.set(
+        f"fs.azure.account.oauth.provider.type.{storage_account}.dfs.core.windows.net",
+        "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider",
+    )
+    spark.conf.set(
+        f"fs.azure.account.oauth2.client.id.{storage_account}.dfs.core.windows.net",
+        app_id,
+    )
+    spark.conf.set(
+        f"fs.azure.account.oauth2.client.secret.{storage_account}.dfs.core.windows.net",
+        app_secret,
+    )
+    spark.conf.set(
+        f"fs.azure.account.oauth2.client.endpoint.{storage_account}.dfs.core.windows.net",
+        f"https://login.microsoftonline.com/{directory_id}/oauth2/token",
+    )
+
+
+def get_storage_for_workspace(workspace_id: str) -> str:
+    """
+    Using a naming convention, returns the expected name of the storage account in the TRE Workspace.
+
+    :param workspace_id: A Workspace ID (GUID)
+
+    :returns: Returns predefined name of the Workspace storage account
+    """
+    return f"{DEFAULT_STORAGE_ACCOUNT_PREFIX}{workspace_id[-4:]}"
+
+
+if __name__ == "__main__":
+    spark_session = SparkSession.builder.getOrCreate()
+    args = parse_args()
+
+    if not args.dataset_name or not args.workspace_id or not args.query_base64:
+        print("No arguments set")
+        exit(0)
+
+    workspace_storage = get_storage_for_workspace(args.workspace_id)
+    set_up_auth_for_storage_account(spark_session, workspace_storage)
+
+    query = base64.b64decode(args.query_base64).decode("ascii")
+    df = spark_session.sql(query)
+
+    now = datetime.datetime.now()
+    str_now = now.strftime("%Y-%m-%d-%H-%M-%S")
+
+    df.write.format("parquet").mode("overwrite").save(
+        f"abfss://{DEFAULT_CONTAINER_NAME}@{workspace_storage}.dfs.core.windows.net/{args.dataset_name}/{str_now}"
+    )
