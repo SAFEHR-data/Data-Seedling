ae6654d6d0075af8e87d03fe60222fcc254e616a
Tatiana Borisova <tborisova@microsoft.com>
2023-10-23 13:02:52 +0000

    Add header linter

--- patient_notes/src/patient_notes/config.py
+++ patient_notes/src/patient_notes/config.py
@@ -11,6 +11,7 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
+
 from patient_notes.common_types import ColumnType, TableConfig
 
 # Language used for Presidio
